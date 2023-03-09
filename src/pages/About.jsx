57261c889ba2a55ffe506260564bb1447368d6a2
Randy Caballero <caballerorandy6@gmail.com>
2023-03-08 23:26:37 -0600

    project done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 const About = () => {
+  const [user, setUser] = useState("mario");
+
+  if (!user) {
+    return <Navigate to="/" replace={true} />;
+  }
+
   return (
     <div className="about">
       <h2>About Us</h2>
@@ -23,6 +32,8 @@ const About = () => {
         quia illum officia obcaecati provident nulla odio molestiae suscipit
         quasi.
       </p>
+
+      <button onClick={() => setUser(null)}>Logout</button>
     </div>
   );
 };
